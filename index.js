a2e32fbcbce903b012e0a1177582df0086b3a331
hasanalam24 <hasanalam365@gmail.com>
2024-05-21 06:27:53 +0600

    get single data for room details page

--- index.js
+++ index.js
@@ -88,6 +88,13 @@ async function run() {
       res.send(result)
     })
 
+    //get a single room data from db using
+    app.get('/room/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await roomsCollection.findOne(query)
+      res.send(result)
+    })
     // app.post('/rooms',async(req,res)=>{
     //   const result=await roomsCollection.insertOne(express.query)
     // })
